f9514b797463cd64061b3e761e2a5715c5604462
Zhu chenghao <zchao@icrr.u-tokyo.ac.jp>
2024-12-19 21:57:17 +0900

    add requirements

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 from setuptools import setup, find_packages
 
+def parse_requirements(filename):
+    with open(filename, 'r') as file:
+        return file.read().splitlines()
+
 setup(
     name='noobfriend',
     version='0.1.0',
     packages=find_packages(),
+    install_requires=parse_requirements('requirements.txt'),
     entry_points={
         'console_scripts': [
             'nbfriend-noobook = jwstnoobfriend.cli:main'
